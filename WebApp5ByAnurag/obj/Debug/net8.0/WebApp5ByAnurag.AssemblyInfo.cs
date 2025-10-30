5dc40ab6a28c5d30653d055ad2ce67986d4d17f6
ANURAG BHATTARAI <141569428+Anuragbhtri019@users.noreply.github.com>
2025-10-30 16:13:48 +0545

    git add. git commit -m "Docker support  added " git push origin main

--- WebApp5ByAnurag/obj/Debug/net8.0/WebApp5ByAnurag.AssemblyInfo.cs
+++ WebApp5ByAnurag/obj/Debug/net8.0/WebApp5ByAnurag.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp5ByAnurag")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e73037d726d889e9f0d0a007dbb99ecbc4ee84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2aab167b79aaeae0a8a62c95a87670160c7ef3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp5ByAnurag")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp5ByAnurag")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
